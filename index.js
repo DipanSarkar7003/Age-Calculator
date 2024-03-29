28da62e33af019fc5e75ca0a301aaeb6f775ae82
Dipan Sarkar <alexmithun1005@gmail.com>
2024-03-29 23:41:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("hello i am on")
+
+const calculate_button = document.querySelector("#calculate_button")
+let user_birthDate = document.querySelector("#user_birthDate") 
+
+
+
+
+calculate_button.addEventListener("click" , ()=>{
+    console.log(user_birthDate)
+})
